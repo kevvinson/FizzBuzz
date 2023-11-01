849af6a271a8d4860b97b2d2e953ee51e1bbba14
Han Xudong <xhan14@slb.com>
2023-11-01 14:42:57 +0800

    feat: add test for number multiple 3 and 5

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -34,7 +34,7 @@ namespace FizzBuzzTest
         }
 
         [Fact]
-        public void Should_return_Fizz_when_countoff_given_number_multiple_5()
+        public void Should_return_Buzz_when_countoff_given_number_multiple_5()
         {
             //given
             string countResult;
@@ -45,6 +45,20 @@ namespace FizzBuzzTest
 
             //then
             Assert.Equal("Buzz", countResult);
+        }
+
+        [Fact]
+        public void Should_return_FizzBuzz_when_countoff_given_number_multiple_3_and_5()
+        {
+            //given
+            string countResult;
+            FizzBuzzGame game = new FizzBuzzGame();
+
+            //when
+            countResult = game.CountOff(15);
+
+            //then
+            Assert.Equal("FizzBuzz", countResult);
         }
     }
 }
